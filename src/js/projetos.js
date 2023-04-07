322e64ea1f2c82026b1be7f420635961ec854626
Eduardo Lobo <contactedulobo@gmail.com>
2023-04-07 02:39:11 -0300

    update 1.8

--- src/js/projetos.js
+++ src/js/projetos.js
@@ -1,3 +1,13 @@
+/* alternar icon navbar */
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
 /* Scroll section */
 
 let sections = document.querySelectorAll("section");
@@ -19,4 +29,41 @@ window.onscroll = () => {
       });
     }
   });
+  /* Sticky Navbar */
+
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /* remover alternar icon e navbar quando link da navbar for clicado  */
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
+
+/* scroll reveal */
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .skills-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/* typed js */
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Estudante de ADS", "Editor de Vídeos"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
